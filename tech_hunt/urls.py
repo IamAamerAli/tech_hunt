2efac95345d86a3e42f1d8e297156eaf10ffbca8
aamerali <sayyed.aamer.ali@gmail.com>
2019-07-05 14:55:12 +0530

    working on profile

--- tech_hunt/urls.py
+++ tech_hunt/urls.py
@@ -13,4 +13,4 @@ urlpatterns = [
                   path("login/", auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
                   path("logout/", auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
                   # path("userinfo/", user_views.user_information, name='user-information'),
-              ] + static(settings.MEDIA_URL, documnets_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, documnet_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -15,3 +16,9 @@ from django.db import models
 #         return self.title
 # endregion
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
